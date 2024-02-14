37a37d80b3ef9f1e13c5180645468e35dee7744e
LindaKat <lindakt16@gmail.com>
2024-02-14 13:51:33 -0600

    define content schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,17 @@
-import { defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content";
+import { format } from "date-fns";
 
-const postsCollection = defineCollection({});
+const postsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z.string().transform((str) => format(new Date(str), "MMMM d, yyyy")),
+    featured: z.boolean().default(false),
+    image: z.string(),
+    title: z.string(),
+  }),
+});
 
 export const collections = {
   posts: postsCollection,
