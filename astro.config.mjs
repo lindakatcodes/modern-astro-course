423180c4e1f855239a1ce017c765149fe59f349d
LindaKat <lindakt16@gmail.com>
2024-05-17 13:17:29 -0500

    convert to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  site: "https://rhythm.nation"
+  site: "https://rhythm.nation",
+  output: "server",
+  adapter: netlify()
 });
